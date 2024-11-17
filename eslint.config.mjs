d8b5613c3dbe52e961a7ec0f0c5761abeee2aa84
Shahriar-Samir <shabusiness035@gmail.com>
2024-11-17 18:41:02 +0600

    update: eslint and prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,44 @@
 // eslint.config.mjs
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: 'readonly',
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  prettier.configs.recommended,
+
   {
     rules: {
-      eqeqeq: "off",
-      "no-unused-vars": "error",
-      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
-      "@typescript-eslint/no-unused-expressions": [
-        "error",
+      eqeqeq: 'off',
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
         {
-          "allowShortCircuit": true, // Allow short-circuit expressions
-          "allowTernary": true, // Allow ternary expressions
+          allowShortCircuit: true, // Allow short-circuit expressions
+          allowTernary: true, // Allow ternary expressions
         },
       ],
     },
   },
+  {},
+
   {
-    ignores: [".node_modules/*",".env","dist"]
-},
-];
+    ignores: ['.node_modules/*', '.env', 'dist'],
+  },
+]
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app:Application = express()
-
-
+const app: Application = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   const a = 'okay'
   res.send(a)
 })
 
-export default app 
+export default app
